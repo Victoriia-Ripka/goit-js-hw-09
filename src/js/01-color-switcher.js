27decbb904277a649ce771acc2e68c70eebe2fc6
Victiriia Ripka <98872709+Victoriia-Ripka@users.noreply.github.com>
2022-08-14 09:45:32 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+    start: document.querySelector('button[data-start]'),
+    stop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+}
+let idInterval = null;
 
+refs.start.addEventListener('click', changeBackgroundColorInterval);
+refs.stop.addEventListener('click', stopInterval);
+
+
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBackgroundColorInterval() {
+    refs.start.setAttribute('disabled', true);
+    refs.stop.removeAttribute('disabled');
+    idInterval = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    } , 1000);
+}
+
+function stopInterval() {
+    refs.start.removeAttribute('disabled');
+    refs.stop.setAttribute('disabled', true);
+    clearInterval(idInterval);
+}
