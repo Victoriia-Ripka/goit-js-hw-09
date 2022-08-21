4afe0544a9fce2385c9c30d7e19badde9bf2c4d5
Victiriia Ripka <98872709+Victoriia-Ripka@users.noreply.github.com>
2022-08-21 22:41:34 +0300

    task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,65 +1,119 @@
-import flatpickr from "flatpickr";
-import throttle from 'lodash.throttle';
-import "flatpickr/dist/flatpickr.min.css";
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
 
-let usersTimeMs;
-let diffTime;
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+// refs
 const refs = {
-    input: document.querySelector('input'),
-    buttom: document.querySelector('button'),
-    days: document.querySelector('span[data-days]'),
-    hours: document.querySelector('span[data-hours]'),
-    minutes: document.querySelector('span[data-minutes]'),
-    seconds: document.querySelector('span[data-seconds]'),
+  input: document.querySelector('input'),
+  start: document.querySelector('button[data-start]'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
 };
 
+let timerId = null;
+
+Notify.init({
+  width: '300px',
+  position: 'center-top',
+  closeButton: true,
+  clickToClose: true,
+});
+
+// options for flatpickr
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-        usersTimeMs = selectedDates[0].getTime();
-        const defaultTime = (new Date()).getTime();
-        diffTime = usersTimeMs - defaultTime;
-        if(usersTimeMs <= defaultTime){ //доробити алерт
-            alert("Please, choose a date in the future");
-        } else {
-            refs.buttom.removeAttribute('disabled');
-        }
-    },
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    const deltaTimeForPlchdr = selectedDate - new Date();
+    alertOnPastTime(deltaTimeForPlchdr);
+    disableBtnStart(deltaTimeForPlchdr);
+    enableBtnStart(deltaTimeForPlchdr);
+    const timeLeftForPlchdr = convertMs(deltaTimeForPlchdr);
+    render(timeLeftForPlchdr, deltaTimeForPlchdr);
+
+    // Обробник кліку по кнопці старт
+    const startBtnClickHandler = () => {
+      timerId = setInterval(updateTimer, 1000);
+    };
+
+    function updateTimer() {
+      const timeNow = new Date();
+      const deltaTime = selectedDate - timeNow;
+      const timeLeft = convertMs(deltaTime);
+      render(timeLeft, deltaTime);
+    }
+    // ==========EventListener==============
+    refs.start.addEventListener('click', startBtnClickHandler);
+  },
 };
 
-refs.buttom.setAttribute('disabled', true);
 flatpickr(refs.input, options);
-refs.input.addEventListener('input', (event) => {
-    options.onClose;
-});
 
-refs.buttom.addEventListener('click', () => {
-    refs.buttom.setAttribute('disabled', true);
-    const maxTime = convertMs(diffTime);
-    refs.days.textContent = maxTime.days;
-    
-    refs.hours.textContent = maxTime.hours;
-    refs.minutes.textContent = maxTime.minutes;
-    refs.seconds.textContent = maxTime.seconds;
-    throttle(refs.seconds.textContent => {
-        return refs.seconds.textContent -= 1;
-    }, 1000);
-});
+// Функція, щоб відключити кнопку Старт
+const disableBtnStart = time => {
+  if (time < 0) {
+    refs.start.setAttribute('disabled', 'disabled');
+  }
+};
 
+// Функція, щоб включити кнопку Старт
+const enableBtnStart = time => {
+  if (time > 0) {
+    refs.start.removeAttribute('disabled');
+  }
+};
+
+// Створюю функцію, щоб обробляти подію коли користувач обере минулий час
+const alertOnPastTime = time => {
+  if (time < 0) {
+    return Notify.warning('Please choose a date in the future');
+  }
+};
+
+// Створюю функцію, щоб додавати 0 коли число складається з однієї цифри
+function addLeadingZero(value) {
+  return value.toString().length < 2
+    ? value.toString().padStart(2, '0')
+    : value;
+}
+
+// Функцію для концертуванна мілісекунд в час
 function convertMs(ms) {
+  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
+  // Remaining days
   const days = Math.floor(ms / day);
+  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
   return { days, hours, minutes, seconds };
 }
+
+const render = ({ days, hours, minutes, seconds }, deltaTime) => {
+  if (deltaTime > 0) {
+    refs.days.textContent = addLeadingZero(days);
+    refs.hours.textContent = addLeadingZero(hours);
+    refs.minutes.textContent = addLeadingZero(minutes);
+    refs.seconds.textContent = addLeadingZero(seconds);
+  } else {
+    refs.days.textContent = addLeadingZero(0);
+    refs.hours.textContent = addLeadingZero(0);
+    refs.minutes.textContent = addLeadingZero(0);
+    refs.seconds.textContent = addLeadingZero(0);
+  }
+};
